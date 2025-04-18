dda79ffad66ab6c86d8ee9c9a9ce94cffc5ecf3b
Sıla <silagrds58@gmail.com>
2025-04-19 01:50:09 +0300

    Serialization & Deserialization

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace OdevSeri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.BtnJsonOku = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Sienna;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(207, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 212);
+            this.listBox1.TabIndex = 0;
+            // 
+            // BtnJsonOku
+            // 
+            this.BtnJsonOku.BackColor = System.Drawing.Color.RosyBrown;
+            this.BtnJsonOku.Location = new System.Drawing.Point(297, 299);
+            this.BtnJsonOku.Name = "BtnJsonOku";
+            this.BtnJsonOku.Size = new System.Drawing.Size(114, 59);
+            this.BtnJsonOku.TabIndex = 1;
+            this.BtnJsonOku.Text = "Json Oku";
+            this.BtnJsonOku.UseVisualStyleBackColor = false;
+            this.BtnJsonOku.Click += new System.EventHandler(this.BtnJsonOku_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnJsonOku);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "JSONUGETİR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button BtnJsonOku;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OdevSeri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnJsonOku_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string dosyaYolu = @"C:\Users\silam\Desktop\urunler.json";  
+            
+                string jsonMetni = File.ReadAllText(dosyaYolu);
+
+                // JSON verisini nesne listesine dönüştür
+                List<Urun> urunler = JsonConvert.DeserializeObject<List<Urun>>(jsonMetni);
+
+                
+                listBox1.Items.Clear();
+
+                foreach (var urun in urunler)
+                {
+                    listBox1.Items.Add($"Ürün: {urun.UrunAdi} - Fiyat: {urun.Fiyat} TL");
+                }
+
+                MessageBox.Show("JSON dosyasından ürünler başarıyla okundu.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message);
+            }
+        }
+    }
+ }
+
